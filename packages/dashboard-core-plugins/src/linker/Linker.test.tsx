0f6aec22954ac08c1ba16ef3432cac3759640b59
Jason Wang <jasonbln07@gmail.com>
2023-02-10 00:58:48 -0500

    completed linker link and linker util tests

---
+++ packages/dashboard-core-plugins/src/linker/Linker.test.tsx
@@ -0,0 +1,35 @@
+// import React from 'react';
+// import { fireEvent, render, screen } from '@testing-library/react';
+// import Linker from './Linker';
+
+// function makeLinker({
+//   x1 = 10,
+//   x2 = 50,
+//   y1 = 10,
+//   y2 = 10,
+//   isSelected = true,
+//   startColumnType = 'java.lang.String',
+//   id = 'TEST_ID',
+//   className = 'linker-link link-is-selected',
+//   operator = FilterType.startsWith,
+//   onClick = jest.fn(),
+//   onDelete = jest.fn(),
+//   onOperatorChanged = jest.fn(),
+// } = {}) {
+//   return render(
+//     <Linker
+//       x1={x1}
+//       x2={x2}
+//       y1={y1}
+//       y2={y2}
+//       id={id}
+//       className={className}
+//       operator={operator}
+//       isSelected={isSelected}
+//       startColumnType={startColumnType}
+//       onClick={onClick}
+//       onDelete={onDelete}
+//       onOperatorChanged={onOperatorChanged}
+//     />
+//   );
+// }
--- packages/dashboard-core-plugins/src/linker/LinkerOverlayContent.test.tsx
+++ packages/dashboard-core-plugins/src/linker/LinkerOverlayContent.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { PanelManager } from '@deephaven/dashboard';
 import GoldenLayout from '@deephaven/golden-layout';
 import LinkerOverlayContent from './LinkerOverlayContent';
@@ -36,6 +36,29 @@ function mountOverlay({
   );
 }
 
-it('mounts and unmounts LinkerOverlay without crashing', () => {
-  mountOverlay();
+it('calls appropriate functions on button and key presses', async () => {
+  const onLinkDeleted = jest.fn();
+  const onAllLinksDeleted = jest.fn();
+  const onCancel = jest.fn();
+  const onDone = jest.fn();
+  mountOverlay({ onLinkDeleted, onAllLinksDeleted, onCancel, onDone });
+
+  const dialog = screen.getByTestId('linker-toast-dialog');
+  expect(dialog).toHaveTextContent(LINKER_OVERLAY_MESSAGE);
+  const buttons = await screen.findAllByRole('button');
+  expect(buttons).toHaveLength(3);
+
+  const clearAllButton = screen.getByRole('button', { name: 'Clear All' });
+  fireEvent.click(clearAllButton);
+  expect(onAllLinksDeleted).toHaveBeenCalled();
+
+  const doneButton = screen.getByRole('button', { name: 'Done' });
+  fireEvent.click(doneButton);
+  expect(onDone).toHaveBeenCalled();
+
+  fireEvent.keyDown(window, { key: 'Escape' });
+  expect(onCancel).toHaveBeenCalled();
+  fireEvent.keyDown(window, { key: 'Delete' });
+  fireEvent.keyDown(window, { key: 'Backspace' });
+  // expect(onLinkDeleted).toHaveBeenCalledTimes(2);
 });
